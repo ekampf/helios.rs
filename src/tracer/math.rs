d180f08b39a67bb38c3490174f5a089802ff2fd5
Eran Kampf <e.kampf@anchorfree.com>
2019-09-13 13:17:09 -0700

    random_on_unit_sphere

--- src/tracer/math.rs
+++ src/tracer/math.rs
@@ -19,3 +19,20 @@ pub fn random_in_unit_sphere() -> Vector3f {
         }
     }
 }
+
+#[allow(dead_code)]
+pub fn random_on_unit_sphere() -> Vector3f {
+    // Pick a random point in the unit cube where x,y,z all range from -1 to +1.
+    // Reject and try again if point is outside of sphere
+    let mut rng = rand::thread_rng();
+    let one = vec3(1_f64, 1_f64, 1_f64);
+
+    loop {
+        let random_vec = vec3(rng.gen(), rng.gen(), rng.gen());
+        let p = (random_vec * 2_f64) - one;
+
+        if p.magnitude2() < 1_f64 {
+            return p.normalize();
+        }
+    }
+}
